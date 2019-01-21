087d3647b2dcf118f710632377f184815e097989
vyskoutaris <vyskoutaris@hotmail.com>
2019-01-21 12:52:55 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,18 @@
+
 class Dog
- 
-def name = (dog_name)
-  @name = dog_name
-end
-
-def name 
-  @name
-end
-
-def breed = (breed)
-  @breed = breed
-end
-
-def breed 
-  @breed
+  
+  def name=(dogs_name)
+    @name = dogs_name
+  end
+  
+  def name
+    @name 
+  end
+  def breed=(breed)
+    @breed = breed
+  end
+  
+  def breed
+    @breed
+  end 
 end
